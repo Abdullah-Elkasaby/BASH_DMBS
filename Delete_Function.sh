f058cb620154263bcfdce7cc8774fda97adba4e6
Abdullah Elkasaby <97049761+Abdullah-Elkasaby@users.noreply.github.com>
2023-04-09 00:20:02 +0200

    typo with delete_function

--- Delete_Function.sh
+++ Delete_Function.sh
@@ -2,7 +2,7 @@
 
 Delete_Function() {
     read -p "Enter table name: " tableName
-    tableName="$1/tableName"
+    tableName="$1/$tableName"
     if [[ ! -f $tableName ]]; then
         echo "Table '$tableName' does not exist."
         return 1
